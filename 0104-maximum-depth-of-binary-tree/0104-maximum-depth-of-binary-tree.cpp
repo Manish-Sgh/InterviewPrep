140f17dae7da7ea3cf50e2d07b6eaf58f66c2674
Manish <manisksing@gmail.com>
2024-12-31 19:15:07 +0530

    Update 0104-maximum-depth-of-binary-tree.cpp

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -25,3 +25,32 @@ public:
         }
     }
 };
+
+//BFS  Approach  lvl order traversal // 
+int maxDepth_BFS(TreeNode* root) {
+
+	if (root == NULL) {
+		return 0;
+	}
+	queue<TreeNode*> q;
+	q.push(root);
+	int depth = 0;
+
+	while (!q.empty()) {
+		depth++;
+		int size = q.size();
+		for (int i = 0; i < size; i++) {
+			TreeNode* temp = q.front();
+			q.pop();
+			if (temp->left != NULL) {
+				q.push(temp->left);
+			}
+			if(temp->right != NULL) {
+				q.push(temp->right);
+			}
+		}
+	}
+	return depth;
+
+}
+
