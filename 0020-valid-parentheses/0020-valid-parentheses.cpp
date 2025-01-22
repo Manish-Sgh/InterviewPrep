800bbdbeb2ab4495284443c312d5eb5d0246431d
Manish <manisksing@gmail.com>
2025-01-22 17:33:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (98.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	bool isValid(string s) {
+        stack<char> stk;
+        int n = s.length();  // base case // 
+        if (n == 0)
+            return true;
+        if (n % 2 != 0)
+            return false;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            }
+
+            else {
+                if (!stk.empty() && ((stk.top() == '(' && s[i] == ')') || (stk.top() == '{' && s[i] == '}') ||  (stk.top() == '[' && s[i] == ']'))) {
+                    stk.pop(); 
+                }
+                else {
+                    return false; 
+                }
+            }
+        }
+        return stk.empty();
+	}
+};
