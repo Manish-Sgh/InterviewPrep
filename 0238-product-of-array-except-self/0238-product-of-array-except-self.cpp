72aa91cd56095c53409e88dcf4bcf228f4d332b1
Manish <manisksing@gmail.com>
2025-01-28 14:28:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    // TC - 0(N)
-    // SC - 0(N)  to store answer 
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prod(nums.size(), 0);
-        int total = 1 , numberOfZero = 0;
-        int index = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                numberOfZero++;
-                if (numberOfZero == 2) {
-                    break;
-                }
-                index = i;
-            } 
-            else {
-                total = total * nums[i];
-            }
-        }
-        if (numberOfZero == 2) {     // 2 or more zeroes will result in zero only everytime // 
-            return prod;
-        }
-        else if (numberOfZero == 1) {  // If exactly one zero, only the position with the zero gets the  
-            prod[index] = total;       // product of all non-zero elements. 
-            return prod; 
-        }
-        else {
-            for (int i = 0; i < nums.size(); i++) {  // Compute the product of all elements and divide by the current 
-                prod[i] = total / nums[i];          // element for every position. // 
-            }
-        }
-        return prod;
+    
+// TC - 0(N)
+// SC - 0(N)  prefix  * suffix 
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> prefix(n), suffix(n), res(n);
+    prefix[0] = 1;
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i-1] * nums[i - 1];
     }
+    suffix[n - 1] = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        res[i] = prefix[i] * suffix[i];
+    }
+
+    return res;
+}
 };
