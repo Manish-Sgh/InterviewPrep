d19838c3b8aff8586cf20fbc2e9acb8e1bd6cbfe
Manish <manisksing@gmail.com>
2025-02-19 00:08:12 +0530

    Time: 123 ms (6.68%), Space: 104.6 MB (5.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    // TC -  0(1) 
-    // SC - 0(n)
-    int findDuplicate(vector<int>& nums) { 
-    int duplicate = -1;
-    unordered_set<int> s;
-
-    for (int i = 0; i < nums.size(); i++) {
-        if (s.find(nums[i]) != s.end())  {
-            duplicate = nums[i];
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int duplicate = 0;
+        
+        for(int i =0; i<  n; i++) {
+            mp[nums[i]]++;
         }
-        s.insert(nums[i]);
-    }
-    return duplicate;
+
+        for(int i =0; i < n ; i++) {
+            if(mp[nums[i]] >= 2) {
+                duplicate = nums[i];
+                break; 
+            }
+        }
+        return duplicate;
     }
 };
