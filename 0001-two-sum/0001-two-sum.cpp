98143556b799046c7b80c9da56c1def91dde88d2
Manish <manisksing@gmail.com>
2025-02-18 20:34:24 +0530

    Time: 4 ms (61.22%), Space: 14.7 MB (54.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    // TC - 0(N)
+    // SC - 0(N)
     vector<int> twoSum(vector<int>& nums, int target) {
-        // TC - 0(N)
-        // SC - 0(N)
         unordered_map<int, int> umap;
-        for (int i = 0; i < nums.size(); i++) {
-            if (umap.find(target - nums[i]) != umap.end()) {
+        int n = nums.size();
+
+        if(n < 2) {
+            return {};
+        }
+
+        for(int i = 0; i < n ; i++) {
+            if(umap.find(target - nums[i]) != umap.end()) {
                 return {i, umap[target - nums[i]]};
             }
             umap[nums[i]] = i;
