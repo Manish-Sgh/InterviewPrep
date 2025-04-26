54d91f3332857c28c781ccae8becd6b761ca0192
Manish <manisksing@gmail.com>
2025-04-26 18:44:23 +0530

    Time: 0 ms (100%), Space: 18.7 MB (52.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,51 +1,7 @@
 class Solution {
 public:
-    // TC -  0((m*n)*(m+n))
-    // void markRow(vector<vector<int>>& matrix, int n, int m, int i) {
-    //     for (int j = 0; j < m; j++) {
-    //         if (matrix[i][j] != 0) {
-    //             matrix[i][j] = -1;
-    //         }
-    //     }
-    // }
-
-    // void markCol(vector<vector<int>>& matrix, int n, int m, int j) {
-    //     for (int i = 0; i < n; i++) {
-    //         if (matrix[i][j] != 0) {
-    //             matrix[i][j] = -1;
-    //         }
-    //     }
-    // }
-
-    // void setZeroes(vector<vector<int>>& matrix) {
-    //     int n = matrix.size();
-    //     int m = matrix[0].size();
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = 0; j < m; j++) {
-    //             if (matrix[i][j] == 0) {
-    //                 markRow(matrix, n, m, i);
-    //                 markCol(matrix, n, m, j);
-    //             }
-    //         }
-    //     }
-
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = 0; j < m; j++) {
-    //             if (matrix[i][j] == -1) {
-    //                 matrix[i][j] = 0;
-    //             }
-    //         }
-    //     }
-    //     /* for (auto it : matrix) {
-    //          for (auto ele : it) {
-    //              cout << ele << " ";
-    //          }
-    //          cout << "\n";
-    //      }*/
-    // }
-
-    //TC - O(2*(N*M))
-    // SC - O(N) + O(M)
+    // TC - O(2*(N*M))
+    //  SC - O(N) + O(M)
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
