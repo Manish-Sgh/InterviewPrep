f5b550d369b561c83eb384e17866c58e83957586
Manish <manisksing@gmail.com>
2025-06-11 03:50:34 +0530

    Time: 40 ms (73.02%), Space: 46.8 MB (41.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-//Time Complexity --> O(n ^ 2)
-//Space Complexity --> O(1) 
+    // Time Complexity --> O(n)
+    // Space Complexity --> O(n)
     int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefix(n);
+
+        prefix[0] = nums[0];
         int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                } else {
-                    continue;
-                }
-            }
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
         }
+
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            if (prefix[i] == k) {
+                count++;
+            }
+            if (mp.find(prefix[i] - k) != mp.end()) {
+                count = count + mp[prefix[i] - k];
+            }
+            mp[prefix[i]]++;
+        }
+
         return count;
     }
 };
